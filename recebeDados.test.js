c3ec0c9936300cfc80904eedcf0aa01462183859
Lucaszxx <lucasd476@gmail.com>
2024-03-26 22:14:45 -0300

    Teste feito com a finalidade de verificar função

--- recebeDados.test.js
+++ recebeDados.test.js
@@ -2,9 +2,9 @@
 const dadosEstudante = require('./dadosEstudante');
 
 describe('Teste da função recebeDados', () => {
-  test('Verifica se a função processa corretamente o nome "Mellinda Gates" e o RA "228697"', () => {
-    const nome = "Mellinda Gates";
-    const RA = "228697";
+  test('Verifica se a função processa corretamente o nome "Lucas Gomes" e o RA "184016"', () => {
+    const nome = "Lucas Gomes";
+    const RA = "184016";
     const esperado = `Dados recebidos - Nome: ${nome}, RA: ${RA}`;
 
     expect(dadosEstudante(nome, RA)).toBe(esperado);
